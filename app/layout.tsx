dc9bdd3d8d54b3001e8641c8e6b1c9205545b1c9
Supriyo Saha <mm.saha1982@gmail.com>
2023-10-16 02:07:25 +0530

    toaster added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import "./globals.css";
 import type { Metadata } from "next";
+import { Toaster } from "sonner";
 import { Inter } from "next/font/google";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
@@ -42,6 +43,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey="creation-theme-2"
           >
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
