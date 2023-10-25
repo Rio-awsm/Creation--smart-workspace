2bf0b37b58d25c3b2ce3d76e5ed094bd1a185617
Supriyo Saha <mm.saha1982@gmail.com>
2023-10-25 16:54:04 +0530

    blocknote text editor added

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { useMutation, useQuery } from "convex/react";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { Toolbar } from "@/components/toolbar";
@@ -11,12 +14,25 @@ interface DocumentIdPageProps {
   params: {
     documentId: Id<"documents">;
   };
-}
+};
 
-const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+const DocumentIdPage = ({
+  params
+}: DocumentIdPageProps) => {
+  const Editor = useMemo(() => dynamic(() => import("@/components/editor"), { ssr: false }) ,[]);
+
   const document = useQuery(api.documents.getById, {
-    documentId: params.documentId,
+    documentId: params.documentId
   });
+
+  const update = useMutation(api.documents.update);
+
+  const onChange = (content: string) => {
+    update({
+      id: params.documentId,
+      content
+    });
+  };
 
   if (document === undefined) {
     return (
@@ -24,10 +40,10 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
         <Cover.Skeleton />
         <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
           <div className="space-y-4 pl-8 pt-4">
-          <Skeleton className="h-14 w-[50%]"  />
-          <Skeleton className="h-4 w-[80%]"  />
-          <Skeleton className="h-4 w-[40%]"  />
-          <Skeleton className="h-4 w-[60%]"  />
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-4 w-[80%]" />
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-4 w-[60%]" />
           </div>
         </div>
       </div>
@@ -35,17 +51,21 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
   }
 
   if (document === null) {
-    return <div>Not Found</div>;
+    return <div>Not found</div>
   }
 
-  return (
+  return ( 
     <div className="pb-40">
       <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
+        <Editor
+          onChange={onChange}
+          initialContent={document.content}
+        />
       </div>
     </div>
-  );
-};
-
+   );
+}
+ 
 export default DocumentIdPage;
